4e6102108b43aaa753d6a4d8f21bc150d47a767b
francesp05 <francemc@unc.edu>
2024-04-12 14:55:24 -0400

    message

--- exercises/ex07/evaluate_algorithms.py
+++ exercises/ex07/evaluate_algorithms.py
@@ -2,6 +2,7 @@ __author__ = "730672003"
 
 import matplotlib.pyplot as plt
 from exercises.ex07.runtime_analysis_functions import evaluate_runtime
+from exercises.ex07.runtime_analysis_functions import evaluate_memory_usage
 
 START_SIZE: int = 0
 END_SIZE: int = 1000
@@ -10,4 +11,22 @@ END_SIZE: int = 1000
 times = evaluate_runtime("selection_sort", START_SIZE, END_SIZE)
 plt.plot(times)
 plt.title("Runtime Analysis of Selection Sort - francemc")
+plt.show()
+
+
+times = evaluate_runtime("insertion_sort", START_SIZE, END_SIZE)
+plt.plot(times)
+plt.title("Runtime Analysis of Insertion Sort - francemc")
+plt.show()
+
+
+usage = evaluate_memory_usage("selection_sort", START_SIZE, END_SIZE)
+plt.plot(usage)
+plt.title("Memory Usage Analysis of Selection Sort - francemc")
+plt.show()
+ 
+
+usage = evaluate_memory_usage("insertion_sort", START_SIZE, END_SIZE)
+plt.plot(usage)
+plt.title("Memory Usage Analysis of Insertion Sort - francemc")
 plt.show()
